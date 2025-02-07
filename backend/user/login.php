847e114b42c514f9a5d151869ef4f5fc86f0b84d
Elias <79870068+elias-utf8@users.noreply.github.com>
2025-02-07 11:46:19 +0100

    maj API

---
+++ backend/user/login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $api_url_token = "http://127.0.0.1:8000/token";
+
+    // 1. Récupérer le token JWT
+    $token_response = file_get_contents($api_url_token, false, stream_context_create([
+        'http' => [
+            'method' => 'POST',
+            'header' => "Content-Type: application/x-www-form-urlencoded",
+            'content' => http_build_query([
+                'username' => $username,
+                'password' => $password
+            ])
+        ]
+    ]));
+
+    $token_data = json_decode($token_response, true);
+    if (isset($token_data['access_token'])) {
+        $_SESSION['token'] = $token_data['access_token'];
+        echo "Connexion réussie. Vous pouvez maintenant réserver une salle.";
+    } else {
+        echo "Erreur de connexion : Identifiants incorrects.";
+    }
+}
+?>
