485322d4840decac146df50889403af495529843
Elias <elias.lolo.46@gmail.com>
2025-03-20 13:55:38 +0100

    Update SQL

--- poleinfo_api/models/reservation.py
+++ poleinfo_api/models/reservation.py
@@ -5,10 +5,27 @@ from typing import List, Dict, Any
 def get_all_reservations() -> List[Dict[str, Any]]:
     """Récupère toutes les réservations"""
     with get_db_cursor() as cursor:
-        query = """
-        SELECT id_reservation, id_salle, id_matiere, id_creneau, 
-               id_user, duree, date, info
-        FROM reservation
+        query = """SELECT 
+    r.id_reservation, 
+    r.duree, 
+    r.date, 
+    r.info,
+    s.numero AS numero_salle,
+    s.capacite AS capacite_salle,
+    s.type AS type_salle,
+    m.nom AS nom_matiere,
+    c.heure_debut,
+    u.nom AS nom_user,
+    u.prenom,
+    GROUP_CONCAT(cl.nom SEPARATOR ', ') AS noms_classes
+FROM reservation r
+LEFT JOIN salle s ON r.id_salle = s.id_salle
+LEFT JOIN matiere m ON r.id_matiere = m.id_matiere
+LEFT JOIN creneau c ON r.id_creneau = c.id_creneau
+LEFT JOIN user u ON r.id_user = u.id_user
+LEFT JOIN classe_reservation cr ON r.id_reservation = cr.id_reservation
+LEFT JOIN classe cl ON cr.id_classe_grp = cl.id_classe_grp
+GROUP BY r.id_reservation, r.duree, r.date, r.info, s.numero, s.capacite, s.type, m.nom, c.heure_debut, u.nom, u.prenom
         """
         cursor.execute(query)
         return cursor.fetchall()
--- poleinfo_api/sql_test.py
+++ poleinfo_api/sql_test.py
@@ -3,17 +3,39 @@ from mysql.connector import Error
 
 try:
     connection = mysql.connector.connect(
-        host='192.168.8.152',        # Hôte
-        database='poleinfo',      # Nom DB
-        user='root',     # Nom d'utilisateur
-        password='cielPOLEINFO25**' # Mot de passe
+        host='192.168.8.152',
+        database='poleinfo',
+        user='root',
+        password='cielPOLEINFO25**'
     )
 
     if connection.is_connected():
         print("Connexion réussie à la base de données MySQL.")
 
         cursor = connection.cursor()
-        requete = "SELECT * FROM reservation"
+        requete = """SELECT 
+    r.id_reservation, 
+    r.duree, 
+    r.date, 
+    r.info,
+    s.numero AS numero_salle,
+    s.capacite AS capacite_salle,
+    s.type AS type_salle,
+    m.nom AS nom_matiere,
+    c.heure_debut,
+    u.nom AS nom_user,
+    u.prenom,
+    GROUP_CONCAT(cl.nom SEPARATOR ', ') AS noms_classes
+FROM reservation r
+LEFT JOIN salle s ON r.id_salle = s.id_salle
+LEFT JOIN matiere m ON r.id_matiere = m.id_matiere
+LEFT JOIN creneau c ON r.id_creneau = c.id_creneau
+LEFT JOIN user u ON r.id_user = u.id_user
+LEFT JOIN classe_reservation cr ON r.id_reservation = cr.id_reservation
+LEFT JOIN classe cl ON cr.id_classe_grp = cl.id_classe_grp
+GROUP BY r.id_reservation, r.duree, r.date, r.info, s.numero, s.capacite, s.type, m.nom, c.heure_debut, u.nom, u.prenom
+
+"""
         cursor.execute(requete)
         resultats = cursor.fetchall()
 
