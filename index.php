56d76712cbe32ab195c4bc7c56152f452076c52c
Zel-douille <ethanclement2005@gmail.com>
2025-03-20 09:09:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ $response_classe_reservation = file_get_contents($request_classe_reservation);
         <div class="space-y-4">
             <div class="bg-blue-400 text-white p-4 rounded-md cursor-pointer" onclick="toggleDetails('details1')">
                 <p><strong>Matière | Classe concernée | Salle</strong></p>
-                <p class="flex justify-end"><strong>Heure</strong></p>
+                <p class="flex justify-end text-1000"><strong>Heure</strong></p>
                 <p class="text-sm text-blue-900">Prof</p>
                 </div>
             <div id="details1" class="hidden bg-blue-200 p-4 rounded-md">
@@ -60,6 +60,7 @@ $response_classe_reservation = file_get_contents($request_classe_reservation);
             
             <div class="bg-white shadow-md p-4 rounded-md cursor-pointer" onclick="toggleDetails('details2')">
                 <p><strong>Matière | Classe concernée | Salle</strong></p>
+                <p class="flex justify-end"><strong>Heure</strong></p>
                 <p class="text-sm text-gray-500">Prof</p>
             </div>
             <div id="details2" class="hidden bg-gray-200 p-4 rounded-md">
@@ -68,6 +69,7 @@ $response_classe_reservation = file_get_contents($request_classe_reservation);
             
             <div class="bg-blue-400 text-white p-4 rounded-md cursor-pointer" onclick="toggleDetails('details3')">
                 <p><strong>Matière | Classe concernée | Salle</strong></p>
+                <p class="flex justify-end"><strong>Heure</strong></p>
                 <p class="text-sm text-blue-900">Prof</p>
             </div>
             <div id="details3" class="hidden bg-blue-200 p-4 rounded-md">
@@ -76,6 +78,7 @@ $response_classe_reservation = file_get_contents($request_classe_reservation);
             
             <div class="bg-white shadow-md p-4 rounded-md cursor-pointer" onclick="toggleDetails('details4')">
                 <p><strong>Matière | Classe concernée | Salle</strong></p>
+                <p class="flex justify-end"><strong>Heure</strong></p>
                 <p class="text-sm text-gray-500">Prof</p>
             </div>
             <div id="details4" class="hidden bg-gray-200 p-4 rounded-md">
