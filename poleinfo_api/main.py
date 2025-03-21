c035fa8f1824e91fcd3d24bcd4ce0fd2d7a9b764
Elias <elias.lolo.46@gmail.com>
2025-03-21 08:34:23 +0100

    Update main.py

--- poleinfo_api/main.py
+++ poleinfo_api/main.py
@@ -9,9 +9,7 @@ from api.endpoints import auth, users, reservations
 from datetime import datetime
 import locale
 
-locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
-now = datetime.now()
-formatted_date = now.strftime("%d %B")
+
 app = FastAPI(title="PoleInfo API", description="API de gestion des réservations de salles")
 
 # Inclusion des routeurs
@@ -21,6 +19,9 @@ app.include_router(reservations.router, prefix="/reservations", tags=["reservati
 
 @app.get("/")
 def read_root():
+    locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
+    now = datetime.now()
+    formatted_date = now.strftime("%d %B")
     return {"message": f"Bienvenue sur l'API PoleInfo, nous sommes le {formatted_date}"}
 
 if __name__ == "__main__":
