78e680942240dd4c03715e338c1fa8119fe5a3ab
Elias <elias.lolo.46@gmail.com>
2025-03-26 10:48:34 +0100

    add password core

--- poleinfo_api/core/security.py
+++ poleinfo_api/core/security.py
@@ -9,14 +9,6 @@ from core.auth import verify_token
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str) -> str:
-    """Hache un mot de passe en utilisant bcrypt"""
-    return pwd_context.hash(password)
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    """Vérifie si un mot de passe correspond au hachage"""
-    return pwd_context.verify(plain_password, hashed_password)
-
 def create_access_token(user_id: int):
     """Crée un token JWT d'accès"""
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
