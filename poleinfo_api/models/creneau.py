c926f94e3ddd98e2493688019da09fa8054b17fc
Elias <elias.lolo.46@gmail.com>
2025-04-02 10:30:18 +0200

    Update creneau.py

--- poleinfo_api/models/creneau.py
+++ poleinfo_api/models/creneau.py
@@ -6,6 +6,6 @@ def get_all_creneaux() -> List[Dict[int, Any]]:
     with get_db_cursor() as cursor:
         query = "SELECT heure_debut FROM creneau"
         cursor.execute(query)
-    return cursor.fetchall()
-
+        results = cursor.fetchall()
+    return results
 
