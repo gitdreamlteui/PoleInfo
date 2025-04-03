acb68ad3bcd9494cbd257906e0d2ff69656b3a7b
Elias <elias.lolo.46@gmail.com>
2025-04-03 13:55:26 +0200

    Update classes.py

--- poleinfo_api/models/classes.py
+++ poleinfo_api/models/classes.py
@@ -4,7 +4,7 @@ from typing import List, Dict, Any
 
 def get_all_classes() -> List[Dict[int, Any]]:
     with get_db_cursor() as cursor:
-        query = "SELECT nom FROM classes"
+        query = "SELECT nom FROM classe"
         cursor.execute(query)
         results = cursor.fetchall()
     return results
