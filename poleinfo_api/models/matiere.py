e5c735fa965a90ce00fbe1e3c3540480c902e5e7
Zel-douille <ethanclement2005@gmail.com>
2025-04-03 14:24:28 +0200

    Update matiere.py

--- poleinfo_api/models/matiere.py
+++ poleinfo_api/models/matiere.py
@@ -9,3 +9,14 @@ def get_all_matieres() -> List[Dict[int, Any]]:
         results = cursor.fetchall()
     return results
 
+def delete_matieres(nom: str) -> None:
+    with get_db_cursor() as cursor:
+        query = "DELETE FROM matiere WHERE nom = %s"
+        cursor.execute(query, (nom,))
+
+def create_matiere(nom: str) -> None:
+    with get_db_cursor() as cursor:
+        query = "INSERT INTO matiere (nom) VALUES (%s)"
+        cursor.execute(query, (nom,))
+
+
