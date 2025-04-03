5aab40b46a14d68ce573fd78a57522cf5edbe0bf
Zel-douille <ethanclement2005@gmail.com>
2025-04-03 13:47:30 +0200

    Create classes.py

---
+++ poleinfo_api/models/classes.py
@@ -0,0 +1,11 @@
+"""Fonctions d'accès aux données des matières"""
+from db.database import get_db_cursor
+from typing import List, Dict, Any
+
+def get_all_classes() -> List[Dict[int, Any]]:
+    with get_db_cursor() as cursor:
+        query = "SELECT nom FROM classes"
+        cursor.execute(query)
+        results = cursor.fetchall()
+    return results
+
