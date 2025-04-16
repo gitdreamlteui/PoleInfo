f37dfa198f76e92ec732a54d5ea904c975bb8f6b
Zel-douille <ethanclement2005@gmail.com>
2025-04-16 11:12:12 +0200

    Ajout sup user

--- admin/traitement-admin.php
+++ admin/traitement-admin.php
@@ -17,6 +17,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($action == "ajouter_utilisateur") {
         ajouterUtilisateur($_POST);
     }
+    else if ($action == "supprimer_utilisateur"){
+        supprimerUtilisateur($_POST);
+    }
     else if ($action == "supprimer_matiere") {
         supprimerMatiere($_POST);
     }
@@ -96,6 +99,39 @@ function ajouterUtilisateur($data) {
         exit;
     }
 
+function supprimerUtilisateur($data){
+    global $api_url_user, $token;
+
+    $login = htmlspecialchars($data['sup_user']);
+    $user=[
+        "login"=>$login
+    ];
+
+       // Convertir les données en JSON
+       $jsonData = json_encode($user);
+    
+       // Initialiser cURL
+       $ch = curl_init($api_url_user);
+       
+       // Configuration de la requête cURL
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+       curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+       curl_setopt($ch, CURLOPT_HTTPHEADER, [
+           "Authorization: Bearer " . $token,
+           "Content-Type: application/json",
+       ]);
+       
+       // Exécuter la requête
+       $response = curl_exec($ch);
+       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+       $curl_error = curl_error($ch);
+       curl_close($ch);
+   
+       verif_HTTP($http_code);
+           header("Location: interface_admin.php");
+           exit;
+}
 
 function supprimerMatiere($data){
     
