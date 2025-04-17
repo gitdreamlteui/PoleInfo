ba9afc01cd6f12b24a0612d5960dc5bb77e3b88f
Elias <elias.lolo.46@gmail.com>
2025-04-17 08:45:38 +0200

    Update classes.py

--- poleinfo_api/models/classes.py
+++ poleinfo_api/models/classes.py
@@ -18,14 +18,14 @@ def get_classe_by_nom(nom: str) -> Dict[str, Any]:
 
 def remove_classe(numero: str) -> bool:
     with get_db_cursor() as cursor:
-        check_query = "SELECT id_salle_grp FROM classe WHERE nom = %s"
+        check_query = "SELECT id_classe_grp FROM classe WHERE nom = %s"
         cursor.execute(check_query, (numero,))
         salle = cursor.fetchone()
         
         if not salle:
             return False
         
-        classe_id = salle['id_salle_grp']
+        classe_id = salle['id_classe_grp']
             
         delete_query = "DELETE FROM classe WHERE id_salle = %s"
         cursor.execute(delete_query, (classe_id,))
