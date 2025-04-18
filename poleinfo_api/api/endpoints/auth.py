d2087f314903b20147f7cd307a989a1eb969553f
Elias <elias.lolo.46@gmail.com>
2025-04-18 10:56:41 +0200

    Update auth.py

--- poleinfo_api/api/endpoints/auth.py
+++ poleinfo_api/api/endpoints/auth.py
@@ -49,6 +49,8 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
     type_user = user["type"]
     user_login = user["login"]
     user_name = user["nom"]
+    print(f"login {user_login}")
+    print(f"name {user_name}")
 
     token = create_access_token(user["id_user"])
     return {"access_token": token, "token_type": "bearer", "user_type": type_user, "user_name": user_name, "user_login" : user_login}
