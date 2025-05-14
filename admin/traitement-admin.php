37a21a34aa6eecb648fc3b8d62dc74fa997df8c0
Zel-douille <ethanclement2005@gmail.com>
2025-05-14 08:39:38 +0200

    Update traitement-admin.php

--- admin/traitement-admin.php
+++ admin/traitement-admin.php
@@ -15,420 +15,181 @@ $api_url_creneau = getApiUrl('/creneaux/');
 $api_url_classe = getApiUrl('/classes/');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
     $action = $_POST["action"] ?? '';
 
-    if ($action == "ajouter_utilisateur") {
-        ajouterUtilisateur($_POST);
-    }
-    else if ($action == "ajouter_matiere") {
-        ajouterMatiere($_POST);
-    }
-    else if ($action == "ajouter_creneau") {
-        ajouterCreneau($_POST);
-    }
-    else if ($action == "ajouter_classe") {
-        ajouterClasse($_POST);
-    }
-    else if ($action == "ajouter_salle") {
-        ajouterSalle($_POST);
-    }
-    else if ($action == "supprimer_utilisateur"){
-        supprimerUtilisateur($_POST);
-    }
-    else if ($action == "supprimer_matiere"){
-        supprimerMatiere($_POST);
-    }
-    else if ($action == "supprimer_salle"){
-        supprimerSalle($_POST);
-    }
-    else if ($action == "supprimer_creneau"){
-        supprimerCreneau($_POST);
-    }
-    else if ($action == "supprimer_classe"){
-        supprimerClasse($_POST);
+    switch ($action) {
+        case "ajouter_utilisateur":
+            ajouterUtilisateur($_POST);
+            break;
+        case "ajouter_matiere":
+            ajouterMatiere($_POST);
+            break;
+        case "ajouter_creneau":
+            ajouterCreneau($_POST);
+            break;
+        case "ajouter_classe":
+            ajouterClasse($_POST);
+            break;
+        case "ajouter_salle":
+            ajouterSalle($_POST);
+            break;
+        case "supprimer_utilisateur":
+            supprimerUtilisateur($_POST);
+            break;
+        case "supprimer_matiere":
+            supprimerMatiere($_POST);
+            break;
+        case "supprimer_salle":
+            supprimerSalle($_POST);
+            break;
+        case "supprimer_creneau":
+            supprimerCreneau($_POST);
+            break;
+        case "supprimer_classe":
+            supprimerClasse($_POST);
+            break;
     }
 }
 
-function verif_HTTP($code){
+function envoyerRequeteAPI($url, $token, $donnees, $methode = "POST") {
+    $jsonData = json_encode($donnees);
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $methode);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Authorization: Bearer " . $token,
+        "Content-Type: application/json",
+    ]);
+
+    $response = curl_exec($ch);
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $curl_error = curl_error($ch);
+    curl_close($ch);
+
+    verif_HTTP($http_code, $response, $curl_error);
+}
+
+function verif_HTTP($code, $response, $curl_error) {
     if ($code === 201 || $code === 200) {
-        $response_data = json_decode($response, true);
-        
-        $_SESSION['info_message'] = "Opération réalisée avec succès  !";
-        header("Location: interface_admin.php");
-        exit;
+        $_SESSION['info_message'] = "Opération réalisée avec succès !";
     } else {
         $message = "Erreur de l'opération : ";
         if (!empty($response)) {
             $error_data = json_decode($response, true);
-            $message .= isset($error_data['detail']) ? $error_data['detail'] : (isset($error_data['message']) ? $error_data['message'] : 'Code ' . $http_code);
+            $message .= $error_data['detail'] ?? $error_data['message'] ?? 'Code ' . $code;
         } elseif (!empty($curl_error)) {
             $message .= $curl_error;
         } else {
             $message .= 'Code ' . $code;
         }
-        
-        $_SESSION['info_message'] = $message;
 
-        }
+        $_SESSION['info_message'] = $message;
     }
+}
 
 function ajouterUtilisateur($data) {
-    
     global $api_url_user, $token;
 
     $nom = htmlspecialchars($data['nom']);
     $prenom = htmlspecialchars($data['prenom']);
-    $type = htmlspecialchars($data['type']);
+    $type = intval(htmlspecialchars($data['type']));
     $password = htmlspecialchars($data['password']);
     $login = mb_strtolower(mb_substr($prenom, 0, 1) . $nom, 'UTF-8');
-    
-    $user = [    
+
+    $user = [
         "login" => $login,
-        "type" => intval($type),
+        "type" => $type,
         "nom" => $nom,
         "prenom" => $prenom,
         "password" => $password
     ];
-    
-    // Convertir les données en JSON
-    $jsonData = json_encode($user);
-    
-    // Initialiser cURL
-    $ch = curl_init($api_url_user);
-    
-    // Configuration de la requête cURL
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Authorization: Bearer " . $token,
-        "Content-Type: application/json",
-    ]);
-    
-    // Exécuter la requête
-    $response = curl_exec($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    $curl_error = curl_error($ch);
-    curl_close($ch);
 
-    verif_HTTP($http_code);
-        header("Location: interface_admin.php");
-        exit;
-    }
-
-function ajouterMatiere($data){
-    global $api_url_matiere, $token;
-
-    $nom = htmlspecialchars($data['add_matiere']);
-    
-    $matiere = [    
-        "nom" => $nom
-    ];
-    
-    // Convertir les données en JSON
-    $jsonData = json_encode($matiere);
-    
-    // Initialiser cURL
-    $ch = curl_init($api_url_matiere);
-    
-    // Configuration de la requête cURL
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Authorization: Bearer " . $token,
-        "Content-Type: application/json",
-    ]);
-    
-    // Exécuter la requête
-    $response = curl_exec($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    $curl_error = curl_error($ch);
-    curl_close($ch);
-
-    verif_HTTP($http_code);
-        header("Location: interface_admin.php");
-        exit;
+    envoyerRequeteAPI($api_url_user, $token, $user, "POST");
+    header("Location: interface_admin.php");
+    exit;
 }
 
-function ajouterCreneau($data){
-    global $api_url_creneau, $token;
+function ajouterMatiere($data) {
+    global $api_url_matiere, $token;
+    $matiere = ["nom" => htmlspecialchars($data['add_matiere'])];
+    envoyerRequeteAPI($api_url_matiere, $token, $matiere, "POST");
+    header("Location: interface_admin.php");
+    exit;
+}
 
+function ajouterCreneau($data) {
+    global $api_url_creneau, $token;
     $heure_debut = htmlspecialchars($data['add_creneau']);
     $time = explode(':', $heure_debut);
     $intervalSpec = sprintf('PT%dH%dM', $time[0], $time[1]);
-    $creneau = [    
-        "heure_debut" => $intervalSpec
-    ];
-    
-    // Convertir les données en JSON
-    $jsonData = json_encode($creneau);
-    
-    // Initialiser cURL
-    $ch = curl_init($api_url_creneau);
-    
-    // Configuration de la requête cURL
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Authorization: Bearer " . $token,
-        "Content-Type: application/json",
-    ]);
-    
-    // Exécuter la requête
-    $response = curl_exec($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    $curl_error = curl_error($ch);
-    curl_close($ch);
-
-    verif_HTTP($http_code);
-        header("Location: interface_admin.php");
-        exit;
+    $creneau = ["heure_debut" => $intervalSpec];
+    envoyerRequeteAPI($api_url_creneau, $token, $creneau, "POST");
+    header("Location: interface_admin.php");
+    exit;
 }
 
-function ajouterClasse($data){
+function ajouterClasse($data) {
     global $api_url_classe, $token;
-
-    $nom = htmlspecialchars($data['add_classe']);
-    
-    $classe = [    
-        "nom" => $nom
-    ];
-    
-    // Convertir les données en JSON
-    $jsonData = json_encode($classe);
-    
-    // Initialiser cURL
-    $ch = curl_init($api_url_classe);
-    
-    // Configuration de la requête cURL
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Authorization: Bearer " . $token,
-        "Content-Type: application/json",
-    ]);
-    
-    // Exécuter la requête
-    $response = curl_exec($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    $curl_error = curl_error($ch);
-    curl_close($ch);
-
-    verif_HTTP($http_code);
-        header("Location: interface_admin.php");
-        exit;
+    $classe = ["nom" => htmlspecialchars($data['add_classe'])];
+    envoyerRequeteAPI($api_url_classe, $token, $classe, "POST");
+    header("Location: interface_admin.php");
+    exit;
 }
 
-function ajouterSalle($data){
+function ajouterSalle($data) {
     global $api_url_salle, $token;
-
-    $numero = htmlspecialchars($data['add_salle1']);
-    $type = htmlspecialchars($data['add_salle2']);
-    $capacite = htmlspecialchars($data['add_salle3']);
-    
-    $salle = [    
-        "numero" => $numero,
-        "type" => $type,
-        "capacite" => intval($capacite)
+    $salle = [
+        "numero" => htmlspecialchars($data['add_salle1']),
+        "type" => htmlspecialchars($data['add_salle2']),
+        "capacite" => intval(htmlspecialchars($data['add_salle3']))
     ];
-    
-    // Convertir les données en JSON
-    $jsonData = json_encode($salle);
-    
-    // Initialiser cURL
-    $ch = curl_init($api_url_salle);
-    
-    // Configuration de la requête cURL
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Authorization: Bearer " . $token,
-        "Content-Type: application/json",
-    ]);
-    
-    // Exécuter la requête
-    $response = curl_exec($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    $curl_error = curl_error($ch);
-    curl_close($ch);
-
-    verif_HTTP($http_code);
-        header("Location: interface_admin.php");
-        exit;
+    envoyerRequeteAPI($api_url_salle, $token, $salle, "POST");
+    header("Location: interface_admin.php");
+    exit;
 }
-function supprimerUtilisateur($data){
+
+function supprimerUtilisateur($data) {
     global $api_url_user, $token;
-
-    $login = htmlspecialchars($data['sup_user']);
-    $user=[
-        "login"=>$login
-    ];
-
-       // Convertir les données en JSON
-       $jsonData = json_encode($user);
-    
-       // Initialiser cURL
-       $ch = curl_init($api_url_user);
-       
-       // Configuration de la requête cURL
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-       curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-       curl_setopt($ch, CURLOPT_HTTPHEADER, [
-           "Authorization: Bearer " . $token,
-           "Content-Type: application/json",
-       ]);
-       
-       // Exécuter la requête
-       $response = curl_exec($ch);
-       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-       $curl_error = curl_error($ch);
-       curl_close($ch);
-   
-       verif_HTTP($http_code);
-           header("Location: interface_admin.php");
-           exit;
+    $user = ["login" => htmlspecialchars($data['sup_user'])];
+    envoyerRequeteAPI($api_url_user, $token, $user, "DELETE");
+    header("Location: interface_admin.php");
+    exit;
 }
 
-function supprimerClasse($data){
+function supprimerClasse($data) {
     global $api_url_classe, $token;
-    $nom = htmlspecialchars($data['sup_classe']);
-    $classe=[
-        "nom"=>$nom
-    ];
-
-       // Convertir les données en JSON
-       $jsonData = json_encode($classe);
-    
-       // Initialiser cURL
-       $ch = curl_init($api_url_classe);
-       
-       // Configuration de la requête cURL
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-       curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-       curl_setopt($ch, CURLOPT_HTTPHEADER, [
-           "Authorization: Bearer " . $token,
-           "Content-Type: application/json",
-       ]);
-       
-       // Exécuter la requête
-       $response = curl_exec($ch);
-       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-       $curl_error = curl_error($ch);
-       curl_close($ch);
-   
-       verif_HTTP($http_code);
-           header("Location: interface_admin.php");
-           exit;
+    $classe = ["nom" => htmlspecialchars($data['sup_classe'])];
+    envoyerRequeteAPI($api_url_classe, $token, $classe, "DELETE");
+    header("Location: interface_admin.php");
+    exit;
 }
 
-function supprimerMatiere($data){
-    
+function supprimerMatiere($data) {
     global $api_url_matiere, $token;
+    $matiere = ["nom" => htmlspecialchars($data['sup_matiere'])];
+    envoyerRequeteAPI($api_url_matiere, $token, $matiere, "DELETE");
+    header("Location: interface_admin.php");
+    exit;
+}
 
-    $nom = htmlspecialchars($data['sup_matiere']);
-    $matiere=[
-        "nom"=>$nom
-    ];
-
-       // Convertir les données en JSON
-       $jsonData = json_encode($matiere);
-    
-       // Initialiser cURL
-       $ch = curl_init($api_url_matiere);
-       
-       // Configuration de la requête cURL
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-       curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-       curl_setopt($ch, CURLOPT_HTTPHEADER, [
-           "Authorization: Bearer " . $token,
-           "Content-Type: application/json",
-       ]);
-       
-       // Exécuter la requête
-       $response = curl_exec($ch);
-       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-       $curl_error = curl_error($ch);
-       curl_close($ch);
-   
-       verif_HTTP($http_code);
-           header("Location: interface_admin.php");
-           exit;
-       }
-
-
-function supprimerSalle($data){
+function supprimerSalle($data) {
     global $api_url_salle, $token;
+    $salle = ["numero" => htmlspecialchars($data['sup_salle'])];
+    envoyerRequeteAPI($api_url_salle, $token, $salle, "DELETE");
+    header("Location: interface_admin.php");
+    exit;
+}
 
-    $numero = htmlspecialchars($data['sup_salle']);
-    $salle=[
-        "numero"=>$numero
-    ];
-
-       // Convertir les données en JSON
-       $jsonData = json_encode($salle);
-    
-       // Initialiser cURL
-       $ch = curl_init($api_url_salle);
-       
-       // Configuration de la requête cURL
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-       curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-       curl_setopt($ch, CURLOPT_HTTPHEADER, [
-           "Authorization: Bearer " . $token,
-           "Content-Type: application/json",
-       ]);
-       
-       // Exécuter la requête
-       $response = curl_exec($ch);
-       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-       $curl_error = curl_error($ch);
-       curl_close($ch);
-       verif_HTTP($http_code);
-           header("Location: interface_admin.php");
-           exit;
-       }
-
-
-function supprimerCreneau($data){
+function supprimerCreneau($data) {
     global $api_url_creneau, $token;
     $heure_debut = htmlspecialchars($data['sup_creneau']);
     $time = explode(':', $heure_debut);
     $intervalSpec = sprintf('PT%dH%dM', $time[0], $time[1]);
-    $creneau=[
-        "heure_debut"=>$intervalSpec
-    ];
-       // Convertir les données en JSON
-       $jsonData = json_encode($creneau);
-    
-       // Initialiser cURL
-       $ch = curl_init($api_url_creneau);
-       
-       // Configuration de la requête cURL
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-       curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-       curl_setopt($ch, CURLOPT_HTTPHEADER, [
-           "Authorization: Bearer " . $token,
-           "Content-Type: application/json",
-       ]);
-       
-       // Exécuter la requête
-       $response = curl_exec($ch);
-       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-       $curl_error = curl_error($ch);
-       curl_close($ch);
-   
-       verif_HTTP($http_code);
-        header("Location: interface_admin.php");
-        exit;
-       }
+    $creneau = ["heure_debut" => $intervalSpec];
+    envoyerRequeteAPI($api_url_creneau, $token, $creneau, "DELETE");
+    header("Location: interface_admin.php");
+    exit;
+}
 ?>
